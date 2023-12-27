d5d824e8c39f5a89520b4fdb969990dc9c03c4b0
Burak Taban <burak@cyphergames.com>
2023-12-27 22:23:41 +0300

    fix scan qr

--- AHAP Tester/ViewControllers/ScanViewController.swift
+++ AHAP Tester/ViewControllers/ScanViewController.swift
@@ -1,45 +1,56 @@
-//
-//  ScanViewController.swift
-//  AHAP Tester
-//
-//  Created by Andrea Mazzini on 26/06/2019.
-//  Copyright © 2019 Fancy Pixel. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
-protocol ScanViewControllerDelegate: class {
+protocol ScanViewControllerDelegate: AnyObject {
   func scanViewController(_ controller: ScanViewController, didScan code: String)
 }
 
 class ScanViewController: UIViewController {
   private var captureSession = AVCaptureSession()
   private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-  var delegate: ScanViewControllerDelegate?
+  weak var delegate: ScanViewControllerDelegate?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+    switch AVCaptureDevice.authorizationStatus(for: .video) {
+    case .authorized:
+        setupCamera()
+    case .notDetermined:
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            if granted {
+                DispatchQueue.main.async {
+                    self.setupCamera()
+                }
+            }
+        }
+    default:
+        print("Camera access denied")
+        dismiss(animated: true, completion: nil)
+        return
+    }
+  }
+
+  private func setupCamera() {
+    let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera], mediaType: .video, position: .back)
     
     guard let captureDevice = deviceDiscoverySession.devices.first else {
-      print("Failed to get the camera device")
-      dismiss(animated: true, completion: nil)
-      return
+        print("Failed to get the camera device")
+        dismiss(animated: true, completion: nil)
+        return
     }
     
     do {
-      let input = try AVCaptureDeviceInput(device: captureDevice)
-      captureSession.addInput(input)
-      let captureMetadataOutput = AVCaptureMetadataOutput()
-      captureSession.addOutput(captureMetadataOutput)
-      captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-      captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        let input = try AVCaptureDeviceInput(device: captureDevice)
+        captureSession.addInput(input)
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession.addOutput(captureMetadataOutput)
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
     } catch {
-      print(error)
-      dismiss(animated: true, completion: nil)
-      return
+        print(error)
+        dismiss(animated: true, completion: nil)
+        return
     }
     
     videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
@@ -49,21 +60,37 @@ class ScanViewController: UIViewController {
     
     captureSession.startRunning()
   }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    if !captureSession.isRunning {
+        captureSession.startRunning()
+    }
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    if captureSession.isRunning {
+        captureSession.stopRunning()
+    }
+  }
 }
 
 extension ScanViewController: AVCaptureMetadataOutputObjectsDelegate {
   func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-    if metadataObjects.count == 0 {
+    if metadataObjects.isEmpty {
       return
     }
     
     let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
     
     if metadataObj.type == AVMetadataObject.ObjectType.qr {
-      if let code = metadataObj.stringValue  {
+      if let code = metadataObj.stringValue {
         delegate?.scanViewController(self, didScan: code)
         captureSession.stopRunning()
-        dismiss(animated: true, completion: nil)
+        DispatchQueue.main.async {
+            self.dismiss(animated: true, completion: nil)
+        }
       }
     }
   }
